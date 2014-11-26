157bd7c5973bc05eb7d9c89bc5bcf356ba4a4911
Vitor Mattos <vitor.mattos@vtex.com.br>
2014-11-26 17:26:20 -0200

    new route to get all medias

--- config/proxy.js
+++ config/proxy.js
@@ -5,5 +5,6 @@ INSTAGRAM_CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID
 
 module.exports = function(app) {
   
-  app.get('/', proxy(INSTAGRAM_CLIENT_ID, INSTAGRAM_USER_ID))
+  app.get('/', proxy.firstPage(INSTAGRAM_CLIENT_ID, INSTAGRAM_USER_ID))
+  app.get('/all', proxy.allPages(INSTAGRAM_CLIENT_ID, INSTAGRAM_USER_ID))
 }

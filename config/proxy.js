5ae424f73e0acf090d05b6a76e1e5ba62c0d9114
Vitor Mattos <vitor.mattos@vtex.com.br>
2014-12-09 14:52:07 -0200

    Missing cors header

--- config/proxy.js
+++ config/proxy.js
@@ -8,12 +8,14 @@ module.exports = function(app) {
   app.get('/', proxy.firstPage(INSTAGRAM_CLIENT_ID, INSTAGRAM_USER_ID), function(req, res) {
     var firstPage = req.instagram.firstPage
 
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.send({data: firstPage})
   })
 
   app.get('/all', proxy.allPages(INSTAGRAM_CLIENT_ID, INSTAGRAM_USER_ID), function(req, res) {
     var allPages = req.instagram.allPages
 
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.send(allPages)
   })
 }

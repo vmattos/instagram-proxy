d2842b5d1d2430749f6d99fd60ea9a0d2c54e056
Vitor Mattos <vitor.mattos@vtex.com.br>
2015-04-13 22:43:20 -0300

    Random tags route

--- config/proxy.js
+++ config/proxy.js
@@ -1,4 +1,5 @@
 var proxy = require('connect-instagram-proxy');
+var _ = require('underscore');
 
 INSTAGRAM_USER_ID = process.env.INSTAGRAM_USER_ID;
 INSTAGRAM_CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
@@ -23,7 +24,18 @@ module.exports = function(app) {
   app.get('/tag/:tag', proxy.firstTagPage(INSTAGRAM_ACCESS_TOKEN), function(req, res) {
      var firstTagPage = req.instagram.firstTagPage;
 
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.send(firstTagPage);
+     res.setHeader("Access-Control-Allow-Origin", "*");
+     res.send(firstTagPage);
+  });
+
+  app.get('/tag/random/:tag', proxy.allTagPages(INSTAGRAM_ACCESS_TOKEN), function(req, res) {
+     var allTagPages = req.instagram.allTagPages;
+
+     var mediasNumber = req.query.medias;
+     console.log(allTagPages)
+     var randomMedias = _.sample(allTagPages.data, mediasNumber);
+
+     res.setHeader("Access-Control-Allow-Origin", "*");
+     res.send(randomMedias);
   });
 };
